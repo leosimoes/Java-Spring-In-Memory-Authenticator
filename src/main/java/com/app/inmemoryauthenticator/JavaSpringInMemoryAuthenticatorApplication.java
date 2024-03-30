ed45ea23f8df9098eefcab23ff45ae94f75fd029
Leonardo <leosimoes.engcomp@gmail.com>
2024-03-30 16:14:18 -0300

    Project started

---
+++ src/main/java/com/app/inmemoryauthenticator/JavaSpringInMemoryAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.app.inmemoryauthenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringInMemoryAuthenticatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringInMemoryAuthenticatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/inmemoryauthenticator/JavaSpringInMemoryAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.inmemoryauthenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringInMemoryAuthenticatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
